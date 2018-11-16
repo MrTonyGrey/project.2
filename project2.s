8eca340d8f8ea1836a85964a550474f8aeac201f
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2018-11-16 15:57:56 -0800

    third commit for lines 13-18
    Lines 13 - 18 outputs "Input is empty" if the input is empty

--- project2.s
+++ project2.s
@@ -4,3 +4,15 @@
   userint:    .space  500 # Reserves 500 bytes which equals 125 words
   invalint: .asciiz "Invalid base-N number." # n is invalid so change its value
 .txt
+
+
+exit:
+  li $v0, 10
+  syscall
+  
+#Outputs if the input is empty
+err_empty_input:
+  la $a0, emptint
+  li $v0, 4
+  syscall
+  j exit
