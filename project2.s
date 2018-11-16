35f848fbce69ae288c055a3be026f71b8086505e
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2018-11-16 15:34:51 -0800

    Second commit for lines 2-6
    line 2 - # Prints out if input is empty
    line 3 -# Prints out if input is too long
    line 4 -# Reserves 500 bytes which equals 125 words
    line 5- # n is invalid so change its value

--- project2.s
+++ project2.s
@@ -1,1 +1,6 @@
 .data
+  empint:   .asciiz "Input is empty." # Prints out if input is empty
+  lngint:    .asciiz "Input is too long." # Prints out if input is too long
+  userint:    .space  500 # Reserves 500 bytes which equals 125 words
+  invalint: .asciiz "Invalid base-N number." # n is invalid so change its value
+.txt
