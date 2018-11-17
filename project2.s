8dad1f7a6cab8c2645b17e1ba26c6236e82fa51d
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2018-11-16 19:58:34 -0800

    Update project2.s

--- project2.s
+++ project2.s
@@ -4,7 +4,7 @@
   lngint:    .asciiz "Input is too long." # Prints out if input is too long
   userint:    .space  500 # Reserves 500 bytes which equals 125 words
   invalint: .asciiz "Invalid N number." # n is invalid so change its value
-.txt
+.text
 
 
 exit:
