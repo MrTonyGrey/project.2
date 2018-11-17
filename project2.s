66cee28c7ee2af265bffc5ddedbcbd0b8fb67664
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2018-11-16 16:05:29 -0800

    fourth commit for lines 19-24
    lines 19-24 output "Invalid N number" so user has to change value

--- project2.s
+++ project2.s
@@ -2,7 +2,7 @@
   empint:   .asciiz "Input is empty." # Prints out if input is empty
   lngint:    .asciiz "Input is too long." # Prints out if input is too long
   userint:    .space  500 # Reserves 500 bytes which equals 125 words
-  invalint: .asciiz "Invalid base-N number." # n is invalid so change its value
+  invalint: .asciiz "Invalid N number." # n is invalid so change its value
 .txt
 
 
@@ -11,8 +11,14 @@ exit:
   syscall
   
 #Outputs if the input is empty
-err_empty_input:
+empty_input:
   la $a0, emptint
+  li $v0, 4
+  syscall
+  j exit
+#Outputs if the input is invalid
+invalid_input:
+  la $a0, invalint
   li $v0, 4
   syscall
   j exit
